32e20ae54dd4c892a3a4a2d21265b8545c46057f
yixuan30 <3043451759@qq.com>
2020-08-19 11:31:35 +0800

    base框架

--- service/service_login/src/main/java/com/pactera/login/controller/FzUserController.java
+++ service/service_login/src/main/java/com/pactera/login/controller/FzUserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FzUserController {
     @GetMapping("/test")
     public R test(){
-        return R.ok().data("test","8ddf2");
+        return R.ok().data("test","hello");
     }
 
 }
